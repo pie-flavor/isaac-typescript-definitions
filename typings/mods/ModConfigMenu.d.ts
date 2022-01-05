2aa05abd492870b97378da88d6e933e0da05ced1
Adam Spofford <pieflavor.mc@gmail.com>
2022-01-04 19:11:30 -0800

    Update ModConfigMenu.d.ts

--- typings/mods/ModConfigMenu.d.ts
+++ typings/mods/ModConfigMenu.d.ts
@@ -2,21 +2,119 @@ declare const ModConfigMenu: ModConfigMenuInterface | undefined;
 
 /** @noSelf */
 declare interface ModConfigMenuInterface {
+  Version: number;
+  
   AddSetting(
     categoryName: string,
     subcategoryName?: string,
     setting?: ModConfigMenuSetting,
   ): void;
 
+  AddBooleanSetting(
+    categoryName: string,
+    subcategoryName: string,
+    attribute: string,
+    defaultValue: boolean,
+    displayText: string,
+    displayValueProxies: LuaTable<boolean, string>,
+    info: string,
+    color?: string,
+  );
+
+  AddNumberSetting(
+    categoryName: string,
+    subcategoryName: string,
+    attribute: string,
+    minValue: number,
+    maxValue: number,
+    modifyBy: number,
+    defaultValue: number,
+    displayText: string,
+    displayValueProxies: LuaTable<number, string>,
+    info: string,
+    color?: string,
+  );
+
+  AddScrollSetting(
+    categoryName: string,
+    subcategoryName: string,
+    attribute: string,
+    defaultValue: number,
+    displayText: string,
+    info: string,
+    color?: string,
+  );
+
+  AddKeyboardSetting(
+    categoryName: string,
+    subcategoryName: string,
+    attribute: string,
+    defaultValue: Keyboard,
+    displayText: string,
+    displayDevice: boolean,
+    info: string,
+    color?: string,
+  );
+
+  AddControllerSetting(
+    categoryName: string,
+    subcategoryName: string,
+    attribute: string,
+    defaultValue: Controller,
+    displayText: string,
+    displayDevice: boolean,
+    info: string,
+    color?: string,
+  );
+
+  SimpleAddSetting(
+    settingType: ModConfigMenuOptionType,
+    categoryName: string,
+    subcategoryName: string,
+    attribute: string,
+    minValue: number,
+    maxValue: number,
+    modifyBy: number,
+    defaultValue: any,
+    displayText: string,
+    displayValueProxies: LuaTable<any, string>,
+    displayDevice: boolean,
+    info: string,
+    color?: string,
+    functionName?: string,
+  );
+
+  RemoveSetting(categoryName: string, subcategoryString: string, settingAttribute: string);
+
   AddSpace(categoryName: string, subcategoryName: string): void;
 
   AddText(
     categoryName: string,
     subcategoryName: string,
-    textFunction: () => string,
+    text: string | () => string,
+    color?: string,
   ): void;
 
+  AddTitle(
+    categoryName: string,
+    subcategoryName: string,
+    text: string,
+    color?: string,
+  );
+
   GetCategoryIDByName(categoryName: string): int | undefined;
+
+  UpdateCategory(categoryName: string, categoryData: CategoryData);
+
+  SetCategoryInfo(categoryName: string, info: string);
+
+  RemoveCategory(categoryName: string);
+
+  GetSubcategoryIDByName(category: string | number, subcategoryName: string): number;
+
+  UpdateSubcategory(categoryName: string, subcategoryName: string, subcategoryData: SubcategoryData);
+
+  RemoveSubcategory(categoryName: string, subcategoryName: string);
 
   Config: {
     General: {
@@ -72,6 +170,17 @@ declare const enum ModConfigMenuOptionType {
   KEYBIND_KEYBOARD = 6,
   KEYBIND_CONTROLLER = 7,
   TITLE = 8,
+}
+
+declare interface CategoryData {
+  Name: string;
+  Info: string;
+  IsOld: boolean;
+}
+
+declare interface SubcategoryData {
+  Name: string;
+  Info: string;
 }
 
 declare interface MenuData {
